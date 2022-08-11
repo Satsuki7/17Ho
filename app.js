12c33d4475773f13d7edb8e1f0c13ccaff42748a
d0r4q <100819284+d0r4q@users.noreply.github.com>
2022-08-11 23:10:18 +0900

    20220811 add cookie-session

--- app.js
+++ app.js
@@ -3,18 +3,15 @@ const express = require("express"),
   http = require("http"),
   path = require("path"),
   routes = require("./routes"),
-  socketIO = require("socket.io")
+  socketIO = require("socket.io"),
   cookieSession = require("cookie-session"),
   //cookieParser = require("cookie-parser")　cookie-sessionダメならこっち使う
 
-  Room = require("./public/javascripts/room.js") //将来的に複数部屋立てる時は書く場所と書き方変わる
-;
-
+  Room = require("./public/javascripts/room.js"); //将来的に複数部屋立てる時は書く場所と書き方変わる
 //サーバ生成
 const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
-server.listen(3000);
 
 //app設定、要勉強
 app.set("views", __dirname + "/views");
@@ -26,26 +23,28 @@ app.set("view engine", "ejs");
 //app.use(express.methodOverride());　express4では不要
 //app.use(app.router);　express4では不要
 //app.use(cookieParser())　とりあえずcookie-session使ってみる
-app.use(cookieSession({
-  name: "session", //セッションの名前
-  secret: "gegegear", //暗号化文字列、なんでもOK
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+app.use(
+  cookieSession({
+    name: "session", //セッションの名前
+    secret: "gegegear", //暗号化文字列、なんでもOK
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 //ルーティング
 app.get("/", routes.title);
 app.post("/lobby", routes.lobby);
 
+server.listen(3000);
 //socket.io　クライアントとのやり取り
-io.sockets.on('connection', function (socket) {
-
-  socket.on('enterLobby', function(data){
+io.sockets.on("connection", function (socket) {
+  socket.on("enterLobby", function (data) {
     var playerId = socket.id; //socket.idをプレイヤIDとして使用
     var name = socket.request.session.name; //セッションに格納されている名前を読み込む
-    var playerList = Room.updatePlayerList(playerId,name);
+    var playerList = Room.updatePlayerList(playerId, name);
     socket.request.session.playerId = playerId; //セッションにプレイヤIDを格納
-    io.sockets.emit('updateLobby', playerList); //クライアントにプレイヤリストを渡す
+    io.sockets.emit("updateLobby", playerList); //クライアントにプレイヤリストを渡す
   });
 
   socket.on("disconnect");
